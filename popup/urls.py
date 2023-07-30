f4a75dc046af931a7ed0777bb1c7e656479dfe71
부준혁 <127727927+BooJunhyuk@users.noreply.github.com>
2023-07-30 17:34:25 +0900

    로그인수정

--- popup/urls.py
+++ popup/urls.py
@@ -26,6 +26,18 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title = "POPCON",
+        default_version = "v1",
+        description = "Swagger를 사용한 API 문서입니다",
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/',SignupView.as_view()),
@@ -33,4 +45,10 @@ urlpatterns = [
     path('myinfo/',MyInfo.as_view()),
     path('logout/',LogoutView.as_view()),
     
+    
+    # Swagger url
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    
 ]
+
