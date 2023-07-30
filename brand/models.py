3cd472e899a61faa9ac3cb24f9712acbfc72d40c
donghapark <donghaha12@naver.com>
2023-07-30 20:15:31 +0900

    브랜드기업정보

--- brand/models.py
+++ brand/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Brand(models.Model):
+    brand_name = models.CharField(verbose_name='기업이름',max_length=50)
+    brand_intro=models.TextField(verbose_name='기업소개')
+    brand_subcounts=models.IntegerField(verbose_name='구독수',default=0)
+    def is_staff(self):
+        return self.is_admin
+    
