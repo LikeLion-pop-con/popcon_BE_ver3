2fe499c7ba21f8d569ec9fee94e55abc7d9599a5
donghapark <donghaha12@naver.com>
2023-08-05 23:55:21 +0900

    allowhost+디비그트루

--- popup/settings.py
+++ popup/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_ow=8xg1si44w+lcz@fk*x=xc4q6ld7n$tw4k7*k&8adwqa4&m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['popcon.store']
+#ALLOWED_HOSTS = ['popcon.store']
 #ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'popcon.store']
 
 # Application definition
 
