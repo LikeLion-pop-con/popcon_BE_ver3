4d183ffe82c63922b8724adbb50ab94410618a26
donghapark <donghaha12@naver.com>
2023-08-18 02:30:57 +0900

    모든 post

--- brandpost/views.py
+++ brandpost/views.py
@@ -11,12 +11,10 @@ from .serializers import *
 
 
 
-class Brand_Have_PopupView(APIView):
+class Brand_Have_PostView(APIView):
     id_param = openapi.Parameter('id', openapi.IN_QUERY, description='브랜드 id', required=True, type=openapi.TYPE_INTEGER)
     @swagger_auto_schema(tags=['브랜드의 포스트(광고느낌) post있는브랜드id=(6,26,23,14,7)  /브랜드 id쿼리로  id =브랜드id'], manual_parameters=[id_param])
     
-    # id= openapi.Parameter('id', openapi.IN_QUERY, description='id', required=True, type=openapi.TYPE_INTEGER)
-    # @swagger_auto_schema(tags=['팝업정보_쿼리로 사용 id=팝업id'])
     def get(self, request):
         brand_id1 = request.GET.get('id')        
         brand_post=Brandpost.objects.filter(brand=brand_id1)    
@@ -25,3 +23,12 @@ class Brand_Have_PopupView(APIView):
         return Response(brandpost_serializer.data, status=200)
             
 
+class Brand_PostallList(APIView):
+    @swagger_auto_schema(tags=['모든 포스트(광고느낌)'])
+    
+    def get(self, request):    
+        brand_post=Brandpost.objects.all()
+        brandpost_serializer = BrandPostSerializer(brand_post, many=True)
+
+        return Response(brandpost_serializer.data, status=200)
+            
